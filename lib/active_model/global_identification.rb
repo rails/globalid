25cab12db3345d4c50a4db28dcc702e95f2c3bf6
Larry Lv <larrylv1990@gmail.com>
2014-05-20 13:47:34 +0800

    Memoize global-id instead of creating objects every time.

--- lib/active_model/global_identification.rb
+++ lib/active_model/global_identification.rb
@@ -6,12 +6,12 @@ module ActiveModel
     extend ActiveSupport::Concern
 
     def global_id
-      GlobalID.create(self)
+      @global_id ||= GlobalID.create(self)
     end
     alias gid global_id
 
     def signed_global_id
-      SignedGlobalID.create(self)
+      @signed_global_id ||= SignedGlobalID.create(self)
     end
     alias sgid signed_global_id
   end
