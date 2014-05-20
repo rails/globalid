e1e4245fb9761df9a9850cff4f6b9eaff7c2cb8b
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2014-05-20 14:27:00 +0900

    Initialize activemodel-globalid in ActiveModel::Railtie

--- lib/active_model/railtie.rb
+++ lib/active_model/railtie.rb
@@ -1,5 +1,16 @@
 require 'active_model/global_identification'
 
-ActiveModel::SignedGlobalID.verifier = Rails.application.message_verifier(:signed_global_ids)
+module ActiveModel
+  class Railtie < Rails::Railtie
+    initializer "active_model.globalid" do
+      config.after_initialize do |app|
+        ActiveModel::SignedGlobalID.verifier = Rails.application.message_verifier(:signed_global_ids)
+      end
 
-ActiveRecord::Base.send :include, ActiveModel::GlobalIdentification
+      ActiveSupport.on_load(:active_record) do
+        ActiveRecord::Base.send :include, ActiveModel::GlobalIdentification
+      end
+    end
+  end
+end
+
