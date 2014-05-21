71c300c50c1da90d7b7f2cc0ac27957ad622d547
Abdelkader Boudih <terminale@gmail.com>
2014-05-21 20:42:19 +0000

    Update railtie.rb

--- lib/active_model/railtie.rb
+++ lib/active_model/railtie.rb
@@ -1,7 +1,8 @@
 require 'active_model/global_identification'
 
 module ActiveModel
-  class Railtie < Rails::Railtie
+  # = Active Model GlobalID Railtie
+  class Railtie < Rails::Railtie # :nodoc:
     initializer "active_model.globalid" do
       config.after_initialize do |app|
         ActiveModel::SignedGlobalID.verifier = app.message_verifier(:signed_global_ids)
