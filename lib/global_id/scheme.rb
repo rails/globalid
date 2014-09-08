c1e5f6ebf7e302fee60a09e61d9f8cd6aa3b1974
Sebastian Sogamoso <sebasoga@gmail.com>
2014-09-07 20:22:45 -0500

    Extract scheme to a constant in URI::GlobalID

--- lib/global_id/scheme.rb
+++ lib/global_id/scheme.rb
@@ -4,38 +4,31 @@ require 'active_support/core_ext/object/blank'
 
 module URI
   # The GlobalID URI scheme
-  #
   class GlobalID < Generic
 
     # An Array of the available components for URI::GlobalID
-    #
     COMPONENT = [ :scheme, :app, :model_name, :model_id ].freeze
 
     # A Regexp to match on the URI's path to get :model_name and :model_id out
     # of it
-    #
     PATH_REGEXP = %r(\A/([^/]+)/?([^/]+)?\z)
 
     # A String that represents the scheme component for GlobalID URIs
-    #
     SCHEME = 'gid'
 
     # Returns the app component of the URI.
     #
     #   URI("gid://bcx/Person/1234").app #=> "bcx"
-    #
     attr_reader :app
 
     # Returns the model_name component of the URI.
     #
     #   URI("gid://bcx/Person/1234").model_name #=> "Person"
-    #
     attr_reader :model_name
 
     # Returns the model_name component of the URI.
     #
     #   URI("gid://bcx/Person/1234").model_id #=> "1234"
-    #
     attr_reader :model_id
 
     # If the value passed as an argument is invalid ArgumentError will be raised
@@ -46,7 +39,6 @@ module URI
     #
     #   URI::GlobalID.validate_app('foo-bar') #=> 'foo-bar'
     #   URI::GlobalID.validate_app('foo@bar') #=> 'bar'
-    #
     def self.validate_app(value)
       parse("gid://#{value}/Person/1234").app
     rescue URI::InvalidComponentError, URI::InvalidURIError
@@ -60,7 +52,6 @@ module URI
     #   app = 'bcx'
     #   person = Person.create(id: 1234)
     #   URI::GlobalID.create('bcx', person) #=> #<URI::GlobalID:0x007ff0b5979138 URL:gid://bcx/Person/1234>
-    #
     def self.create(app, model)
       parse("gid://#{app}/#{model.class.name}/#{model.id}")
     end
@@ -68,7 +59,6 @@ module URI
     # Returns a URI::GlobalID instance from the String passed as an argument
     #
     #   URI::GlobalID.parse('gid://bcx/Person/1234) #=> #<URI::GlobalID:0x007ff0b5979138 URL:gid://bcx/Person/1234>
-    #
     def self.parse(uri)
       uri_components = URI.split(uri)
       build(*uri_components)
@@ -77,7 +67,6 @@ module URI
     # Creates a new URI::GlobalID instance from components of URI::Generic
     # with check. Generic components are: scheme, userinfo, host, port,
     # registry, path, opaque, query and fragment
-    #
     def self.build(*args)
       args << nil   # parser
       args << true  # arg_check
@@ -86,7 +75,6 @@ module URI
     end
 
     # Creates a new URI::Generic instance from generic components without check
-    #
     def initialize(*args)
       @arg_check = args[10]
       args[10] = validate_components?
@@ -106,7 +94,6 @@ module URI
     #
     #   gid = URI::GlobalID.parse('gid://bcx/Person/1234')
     #   gid.app = 'app' #=>  "app"
-    #
     def app=(value)
       validate_component(value) if validate_components?
       check_host(value) if validate_components?
@@ -118,7 +105,6 @@ module URI
     #
     #   gid = URI::GlobalID.parse('gid://bcx/Person/1234')
     #   gid.model_name = 'Person' #=>  "Person"
-    #
     def model_name=(value)
       validate_component(value) if validate_components?
       @model_name = value
@@ -129,7 +115,6 @@ module URI
     #
     #   gid = URI::GlobalID.parse('gid://bcx/Person/1234')
     #   gid.model_id = '1234' #=>  "1234"
-    #
     def model_id=(value)
       validate_component(value) if validate_components?
       @model_id = value
