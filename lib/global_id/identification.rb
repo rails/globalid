a7168e2531d0b20506648536a9422e8a841cbb75
Mehmet Emin İNAÇ <mehmetemininac@gmail.com>
2015-02-25 15:52:45 +0200

    relation_gid, relation_global_id, relation_sgid, relation_signed_global_id methods added into active_record objects

--- lib/global_id/identification.rb
+++ lib/global_id/identification.rb
@@ -21,5 +21,31 @@ class GlobalID
     def to_sgid_param(options = {})
       to_signed_global_id(options).to_param
     end
+
+    ActiveRecord::Associations::Builder::BelongsTo.instance_eval do
+      def define_accessors(model, reflection)
+        class_name = reflection.options.fetch(:class_name, reflection.name.capitalize.to_s)
+        foreign_key = reflection.options.fetch(:foreign_key, "#{reflection.name}_id")
+
+        if reflection.options[:polymorphic]
+          class_name = "#{reflection.name}_type"
+        end
+
+        ["#{reflection.name}_gid", "#{reflection.name}_global_id"].each do |method|
+          model.send(:define_method, method) do
+            GlobalID.create_from(reflection.options[:polymorphic] ? send(class_name) : class_name, send(foreign_key))
+          end
+        end
+
+        ["#{reflection.name}_sgid", "#{reflection.name}_signed_global_id"].each do |method|
+          model.send(:define_method, method) do
+            SignedGlobalID.create_from(reflection.options[:polymorphic] ? send(class_name) : class_name, send(foreign_key))
+          end
+        end
+
+        super
+      end
+    end
+
   end
 end
