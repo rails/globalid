fafdeb5fce2a0dbba98bf9adc8dbcdaf4963d580
zzak <zzakscott@gmail.com>
2023-08-08 18:35:45 +0900

    Fix MessageVerifier to work with new API
    This API changed in rails/rails@91b2a15 (#47325)
    
    /cc @jonathanhefner
    
    How to reproduce:
    
    ```
    >> GlobalID::Locator.locate_signed(Company.first.to_sgid(for: :debug), for: :debug)
      Company Load (0.1ms)  SELECT "companies".* FROM "companies" ORDER BY "companies"."id" ASC LIMIT ?  [["LIMIT", 1]]
      Company Load (0.1ms)  SELECT "companies".* FROM "companies" WHERE "companies"."id" = ? LIMIT ?  [["id", 1], ["LIMIT", 1]]
    => #<Company:0x00000001052f1560 id: 1, name: "nintendo", created_at: Mon, 07 Aug 2023 10:27:45.223195000 UTC +00:00, updated_at: Mon, 07 Aug 2023 10:27:45.223195000 UTC +00:00>
    ```
    
    Previously, using the JSON method, this would return `nil`.

--- lib/global_id/verifier.rb
+++ lib/global_id/verifier.rb
@@ -3,11 +3,11 @@ require 'active_support/message_verifier'
 class GlobalID
   class Verifier < ActiveSupport::MessageVerifier
     private
-      def encode(data)
+      def sign_encoded(data)
         ::Base64.urlsafe_encode64(data)
       end
 
-      def decode(data)
+      def extract_encoded(data)
         ::Base64.urlsafe_decode64(data)
       end
   end
--- test/cases/verifier_test.rb
+++ test/cases/verifier_test.rb
@@ -8,7 +8,7 @@ class VerifierTest < ActiveSupport::TestCase
   # Marshal.dump serializes the hash used in this test to a different string in older versions of Ruby.
   if RUBY_VERSION > "1.9.3"
     test "generates URL-safe messages" do
-      assert_equal "BAh7B0kiCGdpZAY6BkVUSSInZ2lkOi8vYmN4L1BlcnNvbi8xMTUxODY_ZXhwaXJlc19pbgY7AFRJIg9leHBpcmVzX2F0BjsAVDA=--fa4b8c7a28d213288fdd9b6764a5dd119a18a6fe",
+      assert_equal "BAh7B0kiCGdpZAY6BkVUSSInZ2lkOi8vYmN4L1BlcnNvbi8xMTUxODY/ZXhwaXJlc19pbgY7AFRJIg9leHBpcmVzX2F0BjsAVDA=--ae5e44055262447fdbf5d5d39d5120cfa7d5fbe6",
         @verifier.generate({ "gid" => "gid://bcx/Person/115186?expires_in", "expires_at" => nil })
     end
   else
@@ -20,7 +20,7 @@ class VerifierTest < ActiveSupport::TestCase
 
   test "verifies URL-safe messages" do
     assert_equal({ "gid" => "gid://bcx/Person/115186?expires_in", "expires_at" => nil },
-      @verifier.verify("BAh7B0kiCGdpZAY6BkVUSSInZ2lkOi8vYmN4L1BlcnNvbi8xMTUxODY_ZXhwaXJlc19pbgY7AFRJIg9leHBpcmVzX2F0BjsAVDA=--fa4b8c7a28d213288fdd9b6764a5dd119a18a6fe"))
+      @verifier.verify("BAh7B0kiCGdpZAY6BkVUSSInZ2lkOi8vYmN4L1BlcnNvbi8xMTUxODY/ZXhwaXJlc19pbgY7AFRJIg9leHBpcmVzX2F0BjsAVDA=--ae5e44055262447fdbf5d5d39d5120cfa7d5fbe6"))
   end
 
   test "verifies non-URL-safe messages" do
