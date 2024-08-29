91b77137c1aa6297dc9df2542f6e9912137595ad
Tejas Shetty <tejas-shetty@outlook.com>
2024-08-29 11:26:08 +0530

    GlobalId::Identification::build_global_id

--- test/cases/global_identification_test.rb
+++ test/cases/global_identification_test.rb
@@ -9,11 +9,15 @@ class GlobalIdentificationTest < ActiveSupport::TestCase
   test 'creates a Global ID from self' do
     assert_equal GlobalID.create(@model), @model.to_global_id
     assert_equal GlobalID.create(@model), @model.to_gid
+    assert_equal PersonModel.build_global_id(@model), @model.to_gid
+    assert_equal PersonModel.build_global_id(1), @model.to_global_id
   end
 
   test 'creates a Global ID with custom params' do
     assert_equal GlobalID.create(@model, some: 'param'), @model.to_global_id(some: 'param')
     assert_equal GlobalID.create(@model, some: 'param'), @model.to_gid(some: 'param')
+    assert_equal PersonModel.build_global_id(@model, some: 'param'), @model.to_gid(some: 'param')
+    assert_equal PersonModel.build_global_id(1, some: 'param'), @model.to_global_id(some: 'param')
   end
 
   test 'creates a Global ID with a custom app' do
@@ -22,27 +26,35 @@ class GlobalIdentificationTest < ActiveSupport::TestCase
     assert_equal GlobalID.create(model, app: 'custom-app'), model.to_global_id
     assert_equal GlobalID.create(model, app: 'custom-app'), model.to_gid
     assert_equal GlobalID.create(model, app: 'other-app'), model.to_global_id(app: 'other-app')
+    assert_equal GlobalID.create(model, app: 'custom-app'), CustomAppPersonModel.build_global_id(1)
   end
 
   test 'to_gid delegates to overridden to_global_id' do
     model = OverriddenToGlobalIDPersonModel.new id: 1
 
     assert_equal GlobalID.create(model, app: 'override-app'), model.to_gid
+    assert_equal GlobalID.create(model, app: 'override-app'), OverriddenToGlobalIDPersonModel.build_global_id(1)
   end
 
   test 'creates a signed Global ID from self' do
     assert_equal SignedGlobalID.create(@model), @model.to_signed_global_id
     assert_equal SignedGlobalID.create(@model), @model.to_sgid
+    assert_equal PersonModel.build_signed_global_id(@model), @model.to_sgid
+    assert_equal PersonModel.build_signed_global_id(1), @model.to_signed_global_id
   end
 
   test 'creates a signed Global ID with purpose ' do
     assert_equal SignedGlobalID.create(@model, for: 'login'), @model.to_signed_global_id(for: 'login')
     assert_equal SignedGlobalID.create(@model, for: 'login'), @model.to_sgid(for: 'login')
+    assert_equal PersonModel.build_signed_global_id(@model, for: 'login'), @model.to_sgid(for: 'login')
+    assert_equal PersonModel.build_signed_global_id(1, for: 'login'), @model.to_signed_global_id(for: 'login')
   end
 
   test 'creates a signed Global ID with custom params' do
     assert_equal SignedGlobalID.create(@model, some: 'param'), @model.to_signed_global_id(some: 'param')
     assert_equal SignedGlobalID.create(@model, some: 'param'), @model.to_sgid(some: 'param')
+    assert_equal PersonModel.build_signed_global_id(@model, some: 'param'), @model.to_sgid(some: 'param')
+    assert_equal PersonModel.build_signed_global_id(1, some: 'param'), @model.to_signed_global_id(some: 'param')
   end
 
   test 'creates a signed Global ID with a custom app' do
@@ -50,6 +62,12 @@ class GlobalIdentificationTest < ActiveSupport::TestCase
 
     assert_equal SignedGlobalID.create(model, app: 'custom-app').to_s, model.to_signed_global_id.to_s
     assert_equal SignedGlobalID.create(model, app: 'custom-app').to_s, model.to_sgid.to_s
+    assert_equal SignedGlobalID.create(model, app: 'custom-app'), CustomAppPersonModel.build_signed_global_id(1)
+  end
+
+  test "doesn't create a Global ID if ID is not valid" do
+    assert_raises(ArgumentError) { PersonModel.build_global_id(PersonModel) }
+    assert_raises(ArgumentError) { PersonModel.build_signed_global_id(PersonModel) }
   end
 
   test 'dup should clear memoized to_global_id' do
@@ -66,7 +84,7 @@ class CustomAppPersonModel < PersonModel
 end
 
 class OverriddenToGlobalIDPersonModel < PersonModel
-  def to_global_id(app: 'override-app', **)
-    super
+  def to_global_id(options = {})
+    super(options.merge(app: 'override-app'))
   end
 end
